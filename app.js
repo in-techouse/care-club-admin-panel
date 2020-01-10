0d706581058a9d06d6e91561088e7ee10ab9b756
GEO <salihasaeed999@gmail.com>
2020-01-10 12:56:03 +0500

    login

--- app.js
+++ app.js
@@ -23,17 +23,12 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
@@ -42,7 +37,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render("pages/error");
 });
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
   console.log(
     "Express server listening on port %d in %s mode",
     this.address().port,
