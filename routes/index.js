1b9b2fec6b965242c2aa8265ff7f62cf4e336656
samav <samavianorr@gmail.com>
2020-01-10 13:29:22 +0500

    working on firebase

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var firebase = require('firebase');
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+
+  
+};
+firebase.initializeApp(firebaseConfig);
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('pages/login', {error: ""});
