1d112538b5bec2ca30a9eefbdd44e07f94f69a72
GEO <salihasaeed999@gmail.com>
2020-01-11 13:08:06 +0500

    firebase authentification

--- routes/index.js
+++ routes/index.js
@@ -12,17 +12,45 @@ const firebaseConfig = {
   appId: process.env.APP_ID,
   measurementId: process.env.MEASUREMENT_ID
 
-  
+
 };
 firebase.initializeApp(firebaseConfig);
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('pages/login', {error: ""});
+router.get('/', function (req, res) {
+  res.render('pages/login', { error: "" });
 });
 
 
-router.post('/signin', function(req, res) {
-  res.json("1");
+router.post('/signin', function (req, res) {
+  firebase.auth().signInWithEmailAndPassword(req.body.userEmail, req.body.userPassword).then(are => {
+
+    var id = userEmail.replace("@", "-");
+    id = id.replace(/\./g, "_");
+    firebase.database().ref().child("NGOS").child(id).once('value').then(data => {
+
+      if (data === null || data === undefined || data.val() === null || data.val() ===) {
+        res.render('pages/login', { error: "You are not authorized to login here" });
+      }
+      else {
+        req.session.id=data.val().id;
+        req.session.name=data.val().name;
+        req.session.email=req.body.userEmail;
+        res.json("1");
+      }
+    }
+    }).catch(err => {
+
+      res.render('pages/login', { error: "You are not authorized to login here" })
+
+    });
+
+
+}).catch(e => {
+
+  res.render('pages/login', { error: e.message })
+
+});
+  
 });
 
 module.exports = router;
