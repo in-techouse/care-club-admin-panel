192679287dcd22e039fe87668f5a2c5daf05629c
GEO <salihasaeed999@gmail.com>
2020-01-17 12:56:46 +0500

    working on sidebar

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
-
 var firebase = require('firebase');
+
 router.get("/",function(req,res){
-res.json("Hello admin")
+    res.render("pages/admin/index");
+});
+router.get("/allngos",function(req,res){
+    res.render("pages/admin/allngos");
+});
+router.get("/alluser",function(req,res){
+    res.render("pages/admin/alluser");
+});
+router.get("/ngosdetail",function(req,res){
+    res.render("pages/admin/ngosdetail");
+});
+router.get("/userdetail",function(req,res){
+    res.render("pages/admin/userdetail");
 });
 
 
