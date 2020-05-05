06d1a8fd66a1df8b35ed5000926c4cbce036e8c0
samav <samavianorr@gmail.com>
2020-05-05 11:56:49 +0500

    working on ngo side bar

--- routes/ngo.js
+++ routes/ngo.js
@@ -20,7 +20,7 @@ router.get("/allproducts", function (req, res) {
   }
 });
 
-// Action All Products
+// Action My Funds
 router.get("/myfunds", function (req, res) {
   if (req.session.isNGO && req.session.isNGO === true) {
     res.render("pages/ngos/myfunds"); // render page
@@ -28,6 +28,55 @@ router.get("/myfunds", function (req, res) {
     res.redirect("/");
   }
 });
+// Action My Profile
+router.get("/myprofile", function (req, res) {
+  if (req.session.isNGO && req.session.isNGO === true) {
+    res.render("pages/ngos/myprofile"); // render page
+  } else {
+    res.redirect("/");
+  }
+});
+// Action Product Detail
+router.get("/productdetail", function (req, res) {
+  if (req.session.isNGO && req.session.isNGO === true) {
+    res.render("pages/ngos/productdetail"); // render page
+  } else {
+    res.redirect("/");
+  }
+});
+// Action Add Payment Method
+router.get("/adPayment", function (req, res) {
+  if (req.session.isNGO && req.session.isNGO === true) {
+    res.render("pages/ngos/adPayment"); // render page
+  } else {
+    res.redirect("/");
+  }
+});
+// Action My Payment
+router.get("/mypayment", function (req, res) {
+  if (req.session.isNGO && req.session.isNGO === true) {
+    res.render("pages/ngos/mypayment"); // render page
+  } else {
+    res.redirect("/");
+  }
+});
+// Action Add Rider
+router.get("/addrider", function (req, res) {
+  if (req.session.isNGO && req.session.isNGO === true) {
+    res.render("pages/ngos/addrider"); // render page
+  } else {
+    res.redirect("/");
+  }
+});
+// Action My Rider
+router.get("/myrider", function (req, res) {
+  if (req.session.isNGO && req.session.isNGO === true) {
+    res.render("pages/ngos/myrider"); // render page
+  } else {
+    res.redirect("/");
+  }
+});
+
 
 // My profile
 // Product detail
